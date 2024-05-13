b776cca5673ce48ed46b771a412cee62f421b715
chagchagchag <chagchagchag.dev@gmail.com>
2024-05-13 09:47:14 +0900

    의존성 정리

--- example-member-svc/build.gradle.kts
+++ example-member-svc/build.gradle.kts
@@ -24,7 +24,6 @@ repositories {
 extra["springCloudVersion"] = "2023.0.1"
 
 dependencies {
-  implementation("org.springframework.cloud:spring-cloud-starter-gateway")
   implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client")
   implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.5.0")
   compileOnly("org.projectlombok:lombok")
