bfd4eebd9cc64df0c59ccc60d64ed75f44702e7e
chagchagchag <chagchagchag.dev@gmail.com>
2024-08-10 11:46:02 +0900

    chore : 의존성 정리

--- 4-1-with-security-simple/gateway-service/build.gradle.kts
+++ 4-1-with-security-simple/gateway-service/build.gradle.kts
@@ -24,10 +24,29 @@ repositories {
 extra["springCloudVersion"] = "2023.0.1"
 
 dependencies {
+  // webflux
+  implementation("org.springframework.boot:spring-boot-starter-webflux")
+  // security
+  implementation("org.springframework.boot:spring-boot-starter-security")
+  // reactive data
+  implementation("org.springframework.boot:spring-boot-starter-data-reactive")
+
+  // gateway
   implementation("org.springframework.cloud:spring-cloud-starter-gateway")
+
+  // jwt
+  implementation("io.jsonwebtoken:jjwt-api:0.11.2")
+  implementation("io.jsonwebtoken:jjwt-impl:0.11.2")
+  implementation("io.jsonwebtoken:jjwt-jackson:0.11.2")
+
+  // springdoc
   implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.5.0")
+
+  // lombok
   compileOnly("org.projectlombok:lombok")
   annotationProcessor("org.projectlombok:lombok")
+
+  // test
   testImplementation("org.springframework.boot:spring-boot-starter-test")
   testImplementation("io.projectreactor:reactor-test")
 }
