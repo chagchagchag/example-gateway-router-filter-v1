23ea7e849590109fe92137bde12b766ce12b65da
chagchagchag <chagchagchag.dev@gmail.com>
2024-08-14 23:47:03 +0900

    fix : @RestController 누락된 것 추가

--- 4-1-with-security-simple/gateway-service/src/main/java/io/chagchagchag/example/rewrite_path_router_example/gateway/domain/member/MemberController.java
+++ 4-1-with-security-simple/gateway-service/src/main/java/io/chagchagchag/example/rewrite_path_router_example/gateway/domain/member/MemberController.java
@@ -16,8 +16,10 @@ import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
+@RestController
 @RequiredArgsConstructor
 @RequestMapping("/member")
 public class MemberController {
